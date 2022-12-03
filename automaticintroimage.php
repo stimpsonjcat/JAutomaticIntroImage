17003207559a97ef003adea72a35b15c5932af0e
Nicolas Derumigny <nderumigny@gmail.com>
2022-12-03 16:43:23 -0700

    All: allowing to chose first article image

--- automaticintroimage.php
+++ automaticintroimage.php
@@ -19,13 +19,13 @@ class plgContentAutomaticIntroImage extends JPlugin
         * @since  3.1
         */
         protected $autoloadLanguage = true;
-        
+
         /**
         * Automatic creation of resized intro image from article full image
         *
         * @param   string   $context  The context of the content being passed to the
         plugin.
-        * @param   mixed    $article  The JTableContent object that is 
+        * @param   mixed    $article  The JTableContent object that is
         being saved which holds the article data.
         * @param   boolean  $isNew    A boolean which is set to true if the content
         is about to be created.
@@ -41,9 +41,9 @@ class plgContentAutomaticIntroImage extends JPlugin
                 {
                         return true;
                 }
-                
+
                 $images = json_decode($article->images);
-                
+
                 // Check ImageMagick
                 if (!extension_loaded('imagick'))
                 {
@@ -51,24 +51,43 @@ class plgContentAutomaticIntroImage extends JPlugin
                         return true;
                 }
 
-                // Return if full article image is not set or empty
-                if (!isset($images->image_fulltext) or empty($images->image_fulltext))
-                {
-                        return true;
-                }
-                
                 // Return if intro image is already set
                 if (isset($images->image_intro) and !empty($images->image_intro))
                 {
                         Factory::getApplication()->enqueueMessage(JText::_('PLG_CONTENT_AUTOMATICINTROIMAGE_MESSAGE_ALREADY_SET'), 'notice');
                         return true;
                 }
-                
-               
+
+                if ($this->params->get('UseFirstImage'))
+                {
+                    $dom = new DOMDocument();
+                    $dom->loadHTML($article->introtext);
+                    $all_images = $dom->getElementsByTagName('img');
+                    if (count($all_images) > 0)
+                    {
+                        $src_img = $all_images->item(0)->getAttribute('src');
+                        $src_alt = $all_images->item(0)->getAttribute('alt');
+                        $src_caption = "";
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    if (!isset($images->image_fulltext) or empty($images->image_fulltext))
+                    {
+                        return true;
+                    }
+                    $src_img = $images->image_fulltext;
+                }
+
+
                 $width = (int)$this->params->get('Width');
                 $height = (int)$this->params->get('Height');
                 $compression_level = (int)$this->params->get('ImageQuality');
-                
+
                 // Check plugin settings
                 if ($compression_level < 50 OR $compression_level > 100 OR
                     $width < 10 OR $width > 2000 OR
@@ -77,10 +96,10 @@ class plgContentAutomaticIntroImage extends JPlugin
                         Factory::getApplication()->enqueueMessage(JText::_('PLG_CONTENT_AUTOMATICINTROIMAGE_MESSAGE_SETTINGS_ERROR'), 'error');
                         return true;
                 }
-                
+
                 // Create resized image
-                $thumb = new Imagick(JPATH_ROOT . '/' . $images->image_fulltext);
-                
+                $thumb = new Imagick(JPATH_ROOT . '/' . $src_img);
+
                 $thumb->resizeImage($width,
                                     $height,
                                     Imagick::FILTER_LANCZOS,
@@ -91,19 +110,19 @@ class plgContentAutomaticIntroImage extends JPlugin
                 {
                     $thumb->setImageCompressionQuality($compression_level);
                 }
-                
+
                 if ($this->params->get('SetProgressiveJPG') == 1)
                 {
                     $thumb->setInterlaceScheme(Imagick::INTERLACE_PLANE);
                 }
-                
+
                 // Get real image dimensions if maintain aspect ratio was selected
                 if ($this->params->get('MaintainAspectRatio') == 1)
                 {
                     $width = $thumb->getImageWidth();
                     $height = $thumb->getImageHeight();
                 }
-                
+
                 // Set image intro name
                 // {width} and {height} placeholders are changed to values
                 $suffix = $this->params->get('Suffix');
@@ -114,19 +133,19 @@ class plgContentAutomaticIntroImage extends JPlugin
                                           array($width,$height),
                                           $suffix);
                 }
-                $extension_pos = strrpos($images->image_fulltext, '.');
-                $images->image_intro = substr($images->image_fulltext, 0, $extension_pos) . 
-                                        $suffix . 
-                                        substr($images->image_fulltext, $extension_pos);
-                
+                $extension_pos = strrpos($src_img, '.');
+                $images->image_intro = substr($src_img, 0, $extension_pos) .
+                                        $suffix .
+                                        substr($src_img, $extension_pos);
+
                 // Put the image in a subdir if set to do so
                 if ($this->params->get('PutInSubdir') == 1)
                 {
                     $subdir_pos = strrpos($images->image_intro, '/');
-                    $images->image_intro = substr($images->image_intro, 0, $subdir_pos) . 
+                    $images->image_intro = substr($images->image_intro, 0, $subdir_pos) .
                                         '/' . $this->params->get('Subdir') .
                                         substr($images->image_intro, $subdir_pos);
-                
+
                     // Check if the subdir already exist or create it
                     $img_subdir = JPATH_ROOT . '/' . substr($images->image_intro, 0, strrpos($images->image_intro, '/'));
                     if (!JFolder::exists($img_subdir))
@@ -134,16 +153,16 @@ class plgContentAutomaticIntroImage extends JPlugin
                         JFolder::create($img_subdir);
                     }
                 }
-                
+
                 // Copy Alt and Title fields
-                if ($this->params->get('CopyAltTitle') == 1 and 
-                    ($images->image_fulltext_alt != "" or 
-                    $images->image_fulltext_caption != ""))
+                if ($this->params->get('CopyAltTitle') == 1 and
+                    ($src_alt != "" or
+                    $src_altcaption != ""))
                 {
-                    $images->image_intro_alt = $images->image_fulltext_alt;
-                    $images->image_intro_caption = $images->image_fulltext_caption;
+                    $images->image_intro_alt = $src_alt;
+                    $images->image_intro_caption = $src_caption;
                 }
-                
+
                 // Write resized image if it doesn't exist
                 // and set Joomla object values
                 if (!file_exists(JPATH_ROOT . '/' . $images->image_intro))
@@ -155,13 +174,13 @@ class plgContentAutomaticIntroImage extends JPlugin
                 {
                     Factory::getApplication()->enqueueMessage(JText::sprintf('PLG_CONTENT_AUTOMATICINTROIMAGE_MESSAGE_EXIST', $images->image_intro), 'message');
                 }
-                
+
                 $article->images = json_encode($images);
-                
+
                 $thumb->destroy();
-                
+
                 return true;
         }
-        
+
 }
 ?>
